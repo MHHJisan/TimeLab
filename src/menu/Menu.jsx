8f9e722654797ded15eb13736fd5115594db3997
Jisan <jisan@Macs-MacBook-Pro-2.local>
2023-05-31 23:06:08 +0600

    First init

---
+++ src/menu/Menu.jsx
@@ -0,0 +1,26 @@
+export const Menu = () => {
+    return
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Key Features</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Testiminial</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">FAQ</a>
+            </li>
+            <li className="nav-item nav-btn">
+                <button className="Allbtn" type="submit">Try for free</button>
+            </li>
+        </ul>
+    </div>
+
+}
